2db5ddbc7f7d69897697d9bc362a92eaaf03f893
meikalaheshla <fausto.boshoff@gmail.com>
2022-08-31 16:48:21 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+// PROTOTIPO ARRAY 
+    $faq = [
+        [
+            'question' =>  'fa caldo?',
+            'answer' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, error? Iusto porro voluptatem sed quo officia. Hic, culpa. Quas vero dolor perferendis a veritatis omnis impedit nobis dolorum harum dolore.'     
+           ],
+                [
+                'question' =>  'fa caldo?',
+                'answer' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, error? Iusto porro voluptatem sed quo officia. Hic, culpa. Quas vero dolor perferendis a veritatis omnis impedit nobis dolorum harum dolore.'     
+               ],
+                [
+                    'question' =>  'fa caldo?',
+                    'answer' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, error? Iusto porro voluptatem sed quo officia. Hic, culpa. Quas vero dolor perferendis a veritatis omnis impedit nobis dolorum harum dolore.'   
+                     ],
+                    [
+                        'question' =>  'fa caldo?',
+                        'answer' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, error? Iusto porro voluptatem sed quo officia. Hic, culpa. Quas vero dolor perferendis a veritatis omnis impedit nobis dolorum harum dolore.'   
+                         ],
+                        [
+                            'question' =>  'fa caldo?',
+                            'answer' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, error? Iusto porro voluptatem sed quo officia. Hic, culpa. Quas vero dolor perferendis a veritatis omnis impedit nobis dolorum harum dolore.'
+                        ],
+    
+        ];
+
+
+    var_dump($faq);
+    // PROTOTIPO CICLI
+
+    foreach($faq as $values){
+        $question = $values['question'];
+        $answer =  $values['answer'];
+        $answer_paragraphs = explode('.', $answer);
+        var_dump($answer_paragraphs);
+        foreach($answer_paragraphs as $paragraph){
+            var_dump( $paragraph);
+        }
+
+       
+    }
+
+
+
+
+    ?>
